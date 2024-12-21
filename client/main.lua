33b0b217a3231f5833b530147dbadcd4e5369dcc
Shomy <26857315+ShomyTKD@users.noreply.github.com>
2024-12-21 14:20:56 +0100

    Update vote command

--- client/main.lua
+++ client/main.lua
@@ -15,26 +15,11 @@ RegisterCommand('createpoll', function(source, args, rawCommand)
 end, false)
 
 RegisterNUICallback('createPoll', function(data, cb)
-    print("NUI callback 'createPoll' called") -- Add this log
-    print("Creating poll:", data.title)
-    pollData = data
+    pollData = {
+        title = data.title,
+        type = data.type,
+        options = data.options
+    }
     print("Poll created:", pollData.title)
     cb('ok')
 end)
-
-local function getPlayerInformation(_, cb)
-    local info = lib.callback.await('getplayerInformation')
-    local identifiers = {}
-    for _, identifier in pairs(info.identifiers) do identifiers[identifier:match('([^:]+):')] = identifier:match(':(.+)') end
-    cb({ name = info.name, identifiers = identifiers })
-end
-
-RegisterNUICallback('getplayerInformation', getPlayerInformation)
-
-function IsPlayerAdmin(player)
-    -- Replace this with your actual admin check logic
-    local isAdmin = true
-    return isAdmin
-end
-
-print("NUI callback 'createPoll' registered") -- Add this log
